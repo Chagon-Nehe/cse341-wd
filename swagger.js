6ddab369dead47c4a9b2ea7c4353be8e49af9bf8
Chagon-Nehe <cgondwe@byupathway.edu>
2026-09-19 17:42:42 +0200

    week two project

--- swagger.js
+++ swagger.js
@@ -9,7 +9,7 @@ const swaggerDocument = {
 };
 
 const outputFile = './swagger.json';
-const routes = ['./server.js'];
+const routes = ['./api/routes/index.js'];
 
 /* NOTE: If you are using the express Router, you must pass in the 'routes' only the 
 root file where the route starts, such as index.js, app.js, routes.js, etc ... */
